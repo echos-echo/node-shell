63d07fd6c7ec459e8457eec9ae1e7a53d51ca19a
Eve Cho <e.young.echo@gmail.com>
2022-08-03 15:48:37 -0400

    pwd command added

--- bash.js
+++ bash.js
@@ -3,6 +3,11 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
 
-    process.stdout.write('You typed: ' + cmd);
+    if (cmd === 'pwd') {
+        process.stdout.write(process.cwd());
+    } else {
+        process.stdout.write('You typed: ' + cmd);
+    }
+
     process.stdout.write('\nprompt > ');
 })
