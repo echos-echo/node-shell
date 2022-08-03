6e22a1fef9de0bf52855c3e9224b1052361072e2
Eve Cho <e.young.echo@gmail.com>
2022-08-03 16:21:31 -0400

    ls functional

--- ls.js
+++ ls.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
-const ls = fs.readdir('./', 'utf8', (err, files) => {
-    if (err) {
-        throw err;
-    } else {
-        process.stdout.write(files.join('\n'));
-        process.stdout.write('prompt > ');
-    };
-});
+const ls = () => {
+    fs.readdir('./', 'utf8', (err, files) => {
+        if (err) {
+            throw err;
+        } else {
+            process.stdout.write(files.join('\n'));
+            process.stdout.write('\nprompt > ');
+        };
+    });
+}
 
 module.exports.ls = ls;
